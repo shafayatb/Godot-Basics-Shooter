92ec08ba1981b10336da6810f5b617a9f37ec886
Shafayat Bin Mamun <mamunshafayatbin@yahoo.com>
2024-08-24 23:15:41 +0600

    Area2D, signals and timer

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,1 +1,10 @@
 extends Node2D
+
+
+func _on_area_2d_body_entered(body):
+	print(body)
+	print("body entered")
+
+
+func _on_area_2d_body_exited(body):
+	print("body exited")
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 var speed: int = 500
+var can_laser: bool = true
+var can_grenade: bool = true
 
 func _process(_delta):
 	#input
@@ -8,10 +10,24 @@ func _process(_delta):
 	velocity = direction * speed 
 	
 	#laser
-	if Input.is_action_pressed("primary action"):
+	if Input.is_action_pressed("primary action") and can_laser:
 		print_rich("Lasr shot")
+		can_laser = false
+		$LaserTimer.start(0.5)
 	
-	if Input.is_action_pressed("secondary action"):
+	if Input.is_action_pressed("secondary action") and can_grenade:
 		print_rich("grande thrown")	
+		can_grenade= false
+		$GrenadeTimer.start(2.0)
 		
 	move_and_slide()
+	
+	
+
+
+func _on_laser_timer_timeout():
+	can_laser = true
+
+
+func _on_grenade_timer_timeout():
+	can_grenade = true
