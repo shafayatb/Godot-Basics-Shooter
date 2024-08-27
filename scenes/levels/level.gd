15868131bb2fae70c65cd79d95b2e8ba4d84135a
Shafayat Bin Mamun <mamunshafayatbin@yahoo.com>
2024-08-27 08:10:16 +0600

    Rigidbody physics and play direction

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -7,14 +7,13 @@ func _on_gate_player_entered_gate(body):
 	print("Player has entered gate")
 	print(body)
 
-
-func _on_player_granede(pos):
-	var grenade = grenade_scene.instantiate()
-	grenade.position = pos
-	$Projectiles.add_child(grenade)
-
-
-func _on_player_laser(pos):
+func _on_player_laser(pos, direction):
 	var laser = laser_scene.instantiate()
 	laser.position = pos
 	$Projectiles.add_child(laser)
+	
+func _on_player_granede(pos, direction):
+	var grenade = grenade_scene.instantiate() as RigidBody2D
+	grenade.position = pos
+	grenade.linear_velocity = direction * grenade.speed
+	$Projectiles.add_child(grenade)	
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -4,27 +4,31 @@ var speed: int = 500
 var can_laser: bool = true
 var can_grenade: bool = true
 
-signal laser(position)
-signal granede(position)
+signal laser(position, direction)
+signal granede(position, direction)
 
 func _process(_delta):
 	#input
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed 
 	
+	look_at(get_global_mouse_position())
+	
 	#laser
 	if Input.is_action_pressed("primary action") and can_laser:
 		print_rich("Lasr shot")
 		can_laser = false
 		$LaserTimer.start(0.5)
-		laser.emit(get_marker_position())
+		var player_direction = (get_global_mouse_position() - position).normalized()
+		laser.emit(get_marker_position(), player_direction)
 		
 	
 	if Input.is_action_pressed("secondary action") and can_grenade:
 		print_rich("grande thrown")	
 		can_grenade= false
 		$GrenadeTimer.start(2.0)
-		granede.emit(get_marker_position())
+		var player_direction = (get_global_mouse_position() - position).normalized()
+		granede.emit(get_marker_position(), player_direction)
 		
 	move_and_slide()
 	
