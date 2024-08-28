ebea7de598076ccba4eefce9d6283f3c48b708f0
Shafayat Bin Mamun <mamunshafayatbin@yahoo.com>
2024-08-28 07:53:35 +0600

    Laser direction

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -8,8 +8,10 @@ func _on_gate_player_entered_gate(body):
 	print(body)
 
 func _on_player_laser(pos, direction):
-	var laser = laser_scene.instantiate()
+	var laser = laser_scene.instantiate() as Area2D
 	laser.position = pos
+	laser.rotation_degrees = rad_to_deg(direction.angle()) + 90
+	laser.direction = direction
 	$Projectiles.add_child(laser)
 	
 func _on_player_granede(pos, direction):
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -13,13 +13,12 @@ func _process(_delta):
 	velocity = direction * speed 
 	
 	look_at(get_global_mouse_position())
-	
+	var player_direction = (get_global_mouse_position() - position).normalized()
 	#laser
 	if Input.is_action_pressed("primary action") and can_laser:
 		print_rich("Lasr shot")
 		can_laser = false
 		$LaserTimer.start(0.5)
-		var player_direction = (get_global_mouse_position() - position).normalized()
 		laser.emit(get_marker_position(), player_direction)
 		
 	
@@ -27,7 +26,6 @@ func _process(_delta):
 		print_rich("grande thrown")	
 		can_grenade= false
 		$GrenadeTimer.start(2.0)
-		var player_direction = (get_global_mouse_position() - position).normalized()
 		granede.emit(get_marker_position(), player_direction)
 		
 	move_and_slide()
