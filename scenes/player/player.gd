497dc9d72f1a4a8efec900c2099463b8de46c263
Shafayat Bin Mamun <mamunshafayatbin@yahoo.com>
2024-08-31 00:06:46 +0600

    particle systems.

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -16,6 +16,7 @@ func _process(_delta):
 	var player_direction = (get_global_mouse_position() - position).normalized()
 	#laser
 	if Input.is_action_pressed("primary action") and can_laser:
+		$LaserParticle.emitting = true
 		print_rich("Lasr shot")
 		can_laser = false
 		$LaserTimer.start(0.5)
