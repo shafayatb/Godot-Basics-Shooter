997bfc8796274c7e7b9beb3c2cf487aea3555c61
Shafayat Bin Mamun <mamunshafayatbin@yahoo.com>
2024-09-22 15:08:44 +0600

    Free laser timer

--- scenes/projectiles/laser.gd
+++ scenes/projectiles/laser.gd
@@ -3,6 +3,8 @@ extends Area2D
 @export var speed: int = 1000
 var direction: Vector2 = Vector2.UP
 
+func _ready():
+	$SelfDestructTimer.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -12,4 +14,9 @@ func _process(delta):
 func _on_body_entered(body):
 	if "hit" in body:
 		body.hit()
+	queue_free()
+
+
+func _on_laser_timer_timeout():
+	print("kill laser")
 	queue_free()
