5d740c2860d894179b3142b11203a75bf5059d92
Shafayat Bin Mamun <mamunshafayatbin@yahoo.com>
2024-09-25 21:30:02 +0600

    Godot scene inheritance basics

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,10 +1,9 @@
 extends Node2D
 
+class_name LevelParent
+
 var laser_scene: PackedScene = preload("res://scenes/projectiles/laser.tscn")
 var grenade_scene: PackedScene = preload("res://scenes/projectiles/grenade.tscn")
-
-func _on_gate_player_entered_gate(_body):
-	var _tween = create_tween()
 
 func _on_player_laser(pos, direction):
 	var laser = laser_scene.instantiate() as Area2D
