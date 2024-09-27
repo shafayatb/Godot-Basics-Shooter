a0427dd5a91c126b7b20d7d85e25656d213104c5
Shafayat Bin Mamun <mamunshafayatbin@yahoo.com>
2024-09-27 21:32:40 +0600

    Added call deferred to remove debuggger warning

--- scenes/levels/inside.gd
+++ scenes/levels/inside.gd
@@ -4,4 +4,4 @@ extends LevelParent
 func _on_exit_gate_area_body_entered(_body):
 	var tween = create_tween()
 	tween.tween_property($Player, "speed", 0, 0.5)
-	get_tree().change_scene_to_file("res://scenes/levels/outside.tscn")
+	get_tree().change_scene_to_file.call_deferred("res://scenes/levels/outside.tscn")
--- scenes/levels/outside.gd
+++ scenes/levels/outside.gd
@@ -5,4 +5,4 @@ extends LevelParent
 func _on_gate_player_entered_gate(_body):
 	var tween = create_tween()
 	tween.tween_property($Player, "speed", 0, 0.5)
-	get_tree().change_scene_to_packed(inside_level_scene)
+	get_tree().change_scene_to_packed.call_deferred(inside_level_scene)
