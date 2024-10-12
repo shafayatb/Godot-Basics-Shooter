db33b7c42eeb31be27af3364f2d7bff0e667681b
Shafayat Bin Mamun <mamunshafayatbin@yahoo.com>
2024-10-12 15:38:37 +0600

    Drone hit and increase drone movement speed when target is hit.

--- scenes/enemies/drone.gd
+++ scenes/enemies/drone.gd
@@ -3,11 +3,13 @@ extends CharacterBody2D
 var active: bool = false
 var health := 50
 var will_take_damage := true
-var drone_speed: int = 400
+var max_drone_speed: int = 400
+var drone_speed: int = 0
+var explosion_active: bool = false
 
 func _ready():
 	$Explosion.hide()
-	$Drone.show()
+	$DroneSprite.show()
 
 func _process(delta):
 	if active:
@@ -16,14 +18,17 @@ func _process(delta):
 		velocity = direction * drone_speed * delta
 		var collision = move_and_collide(velocity)
 		if collision:
+			print(collision)
 			kill_drone()
+	if explosion_active:
+		damage_surroundings()		
 	
 func hit():
 	if will_take_damage:
 		health -= 10
 		will_take_damage = false
 		$HitTimer.start()
-		$Drone.material.set_shader_parameter("progress", 1)
+		$DroneSprite.material.set_shader_parameter("progress", 1)
 	if health <=0:
 		kill_drone()
 
@@ -31,11 +36,21 @@ func kill_drone():
 	drone_speed = 0
 	active = false
 	$AnimationPlayer.play("explosion")
+	explosion_active = true
+	
+func damage_surroundings():
+	var targets =  get_tree().get_nodes_in_group("Entity")
+	for target in targets:
+		var in_range = target.global_position.distance_to(global_position) < 400
+		if "hit" in target and in_range:
+			target.hit()
 
 func _on_area_2d_body_entered(_body):
 	active = true
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "drone_speed", max_drone_speed, 5)
 
 
 func _on_hit_timer_timeout():
 	will_take_damage = true
-	$Drone.material.set_shader_parameter("progress", 0)
+	$DroneSprite.material.set_shader_parameter("progress", 0)
