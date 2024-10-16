8a157a666772add275891948f72de79adb3939c0
Shafayat Bin Mamun <mamunshafayatbin@yahoo.com>
2024-10-17 00:26:07 +0600

    hit and attack animations

--- scenes/enemies/hunter.gd
+++ scenes/enemies/hunter.gd
@@ -4,25 +4,38 @@ var active: bool = false
 var speed: int = 300
 
 func _ready():
+	$NavigationAgent2D.path_desired_distance = 4.0
+	$NavigationAgent2D.target_desired_distance = 4.0
 	$NavigationAgent2D.target_position = Globals.player_position
 
 func _physics_process(_delta):
 	if active:
-		look_at(Globals.player_position)
 		var next_path_pos = $NavigationAgent2D.get_next_path_position()
 		var direction = (next_path_pos - global_position).normalized()
 		velocity = direction * speed
 		move_and_slide()
+		var look_angle = direction.angle()
+		rotation = look_angle + PI/2
 	
 
 func _on_notice_area_body_entered(_body):
 	active = true
+	$AnimationPlayer.play("walk")
 
 
 func _on_notice_area_body_exited(_body):
 	active = false
+	$AnimationPlayer.stop()
 
 
 func _on_position_timer_timeout():
 	if active:
 		$NavigationAgent2D.target_position = Globals.player_position
+
+
+func _on_attack_area_body_entered(body):
+	pass # Replace with function body.
+
+
+func _on_attack_area_body_exited(body):
+	pass # Replace with function body.
